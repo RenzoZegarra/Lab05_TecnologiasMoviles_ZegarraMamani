a1b558ff75b5c93afeb82e4c0a725344a292634a
RenzoZegarra <renzolzegarra@gmail.com>
2026-04-15 23:50:18 -0500

    Se agregó la primera actividad.

---
+++ usopreferencias/app/src/main/java/com/renzo/usopreferencias/AppPreferenciasFragment.kt
@@ -0,0 +1,9 @@
+package com.renzo.usopreferencias
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+class AppPreferenciasFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.misapppreferencias, rootKey)
+    }
+}
